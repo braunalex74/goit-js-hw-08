84f5043cf1bfe8523f28978f4321fe39056074a9
braunalex74 <braunalex74@googlemail.com>
2023-03-21 06:46:14 +0200

    DZ_8_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const imageContainer = document.querySelector('.gallery');
+const imagesMarkup = createImagesModalMarkup(galleryItems);
+imageContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+imageContainer.addEventListener('click', onImageClick);
+
+function createImagesModalMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>
+    </div>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function onImageClick(event) {
+  event.preventDefault();
+  const imageDataSet = event.target.dataset.source;
+  if (!imageDataSet) {
+    return;
+  }
+}
 
 console.log(galleryItems);
