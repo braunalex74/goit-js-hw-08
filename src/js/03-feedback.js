0a696622002c715e77df1365e71e9c79a50a6952
braunalex74 <braunalex74@googlemail.com>
2023-03-21 11:54:03 +0200

    DZ_8_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,14 @@
 import throttle from 'lodash.throttle';
 
 const refs = {
-  form: document.querySelector('.js-feedback-form'),
-  textarea: document.querySelector('.js-feedback-form', textarea),
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form', textarea),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', onTextareaInput);
+
+function onTextareaInput(event) {
+  const message = event.currentTarget.value;
+  localStorage.setItem('feedback-form-state', message);
+}
